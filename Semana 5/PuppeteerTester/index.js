d9ac688f064e9a15be0c0d3491cea16c8e90c2cf
Laura Cabra <lh.cabra@uniandes.edu.co>
2023-11-09 23:11:27 -0500

    Fix faker+variables for url, user, pw+ log the results of assertions

--- Semana 5/PuppeteerTester/index.js
+++ Semana 5/PuppeteerTester/index.js
@@ -1,7 +1,10 @@
 const assert = require("assert");
 const puppeteer = require("puppeteer");
-const faker = require("faker");
+const { faker } = require('@faker-js/faker');
 
+const ghostUrl="http://localhost:2369/ghost/";
+const userEmail="prueba@prueba.com";
+const userPassword="prueba12345";
 const fechaHoraActual = new Date();
 
 /**
@@ -16,18 +19,19 @@ const fechaHoraActual = new Date();
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto("http://localhost:2368/ghost/#/signin");
+  await page.goto(ghostUrl);
   await new Promise((r) => setTimeout(r, 5000));
   const idField = await page.$("#identification");
-  await idField.type("pepe@grillo.com");
+  await idField.type(userEmail);
   const passwordField = await page.$("#password");
-  await passwordField.type("pepegrillo");
+  await passwordField.type(userPassword);
   page.click("#ember5");
   await new Promise((r) => setTimeout(r, 5000));
   const url = await page.url();
   await browser.close();
-  assert.equal(url, "http://localhost:2368/ghost/#/dashboard");
-})().catch((e) => console.log(e));
+  assert.equal(url, "http://localhost:2369/ghost/#/dashboard");
+  console.log("E1-Test Passed - Expected: http://localhost:2369/ghost/#/dashboard, Actual: ",url,".");
+})().catch((e) => console.log("Test Failed - Expected: ",e.expected,", Actual: ",e.actual,"."));
 
 /**
  * Escenario 2: Como usuario administrador realizo el inicio sesión en Ghost (negativo)
@@ -41,12 +45,13 @@ const fechaHoraActual = new Date();
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto("http://localhost:2368/ghost/#/signin");
+    await page.goto(ghostUrl);
     await new Promise((r) => setTimeout(r, 5000));
     const testData = [
-        { email: '', password: '', error: 'Please fill out the form to sign in.' },
-        { email: faker.email, password: faker.password, error: 'There is no user with that email address.' }
-      ];
+      { email: '', password: '', error: 'Please fill out the form to sign in.' },
+      { email: faker.internet.email(), password: faker.internet.password(), error: 'There is no user with that email address.' }
+    ];
+    
       for (const data of testData) {
         // Fill in the email and password
         await page.type('#identification', data.email);
@@ -55,17 +60,19 @@ const fechaHoraActual = new Date();
         await page.click('#ember5');
         // Wait for the error message to appear
         await page.waitForSelector('.main-error');
+        await new Promise((r) => setTimeout(r, 5000));
         // Get the actual error message text
         const actualErrorMessage = await page.$eval('.main-error', el => el.textContent.trim());
         // Compare with the expected error message
-        if (actualErrorMessage === data.error) {
-          console.log(`Test Passed - Expected: "${data.error}", Actual: "${actualErrorMessage}"`);
-        } else {
-          console.error(`Test Failed - Expected: "${data.error}", Actual: "${actualErrorMessage}"`);
+        try {
+          assert.equal(actualErrorMessage, data.error);
+          console.log("E2-Test Passed - Expected:", data.error, ", Actual:", actualErrorMessage);
+        } catch (e) {
+          console.error("E2-Test Failed - Expected:", data.error, ", Actual:", actualErrorMessage);
         }
+       
       }
 
-    await new Promise((r) => setTimeout(r, 5000));
     const url = await page.url();
     await browser.close();
 
