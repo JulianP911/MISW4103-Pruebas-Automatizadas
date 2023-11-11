842f3955ca24f707505454ea9aeb9065fa17fec8
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2023-11-11 01:18:43 -0500

    ajuste de test crear post

--- Semana 5/PuppeteerTester/postsPage.js
+++ Semana 5/PuppeteerTester/postsPage.js
@@ -1,3 +1,5 @@
+const { faker } = require("@faker-js/faker");
+
 class PostsPage {
   constructor(page, ghostUrl, screenshotDirectoryEscenario) {
     this.page = page;
@@ -10,14 +12,13 @@ class PostsPage {
       if (elementExists) {
         await this.page.click("#ember313"); // Screen is wide
       } else {
-        if((await this.page.$("#ember13")) !== null){
-        await this.page.click("#ember13"); // Screen is narrow
-      }
+        if ((await this.page.$("#ember13")) !== null) {
+          await this.page.click("#ember13"); // Screen is narrow
+        }
       }
       await this.page.screenshot({
         path: this.screenshotDirectoryEscenario + "postsPage.png",
       });
-    
     } catch (error) {
       console.error("Visit Post Page failed:", error.message);
       throw error; // Rethrow the error to propagate it to the calling code
@@ -25,12 +26,24 @@ class PostsPage {
   }
 
   async createPost() {
-    try {   // Wait for an element that contains a span with the text "New post"
-    await this.page.click('.view-actions-top-row');
-    await this.page.screenshot({
-      path: this.screenshotDirectoryEscenario + "createPostsPage.png",
-    });
-    return this.page; } catch (error) {
+    try {
+      // Wait for an element that contains a span with the text "New post"
+      await this.page.click(".view-actions-top-row");
+      await this.page.waitForTimeout(2000);
+await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.keyboard.press('Tab');
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.waitForTimeout(1000);
+      await Promise.resolve(this.page.click('button[data-test-button="publish-flow"]'));
+      await this.page.waitForTimeout(1000);
+      await Promise.resolve(this.page.click('button[data-test-button="continue"]'));
+      await this.page.waitForTimeout(1000);
+      await Promise.resolve(this.page.click('button[data-test-button="confirm-publish"]'));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "createPostsPage.png",
+      });
+      return this.page;
+    } catch (error) {
       console.error("Visit Post Page failed:", error.message);
       throw error; // Rethrow the error to propagate it to the calling code
     }
