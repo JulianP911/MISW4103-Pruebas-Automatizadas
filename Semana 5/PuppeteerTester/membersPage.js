69f5102ad48c2156bf4b58f57dd4c4f4c0686e49
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2023-11-11 20:13:17 -0500

    Update membersPage.js

--- Semana 5/PuppeteerTester/membersPage.js
+++ Semana 5/PuppeteerTester/membersPage.js
@@ -8,20 +8,18 @@ class MembersPage {
   }
   async visit() {
     try {
-      try{
-      await this.page.waitForSelector('a[data-test-mobile-nav="members"]');
+      try {
+        await this.page.waitForSelector('a[data-test-mobile-nav="members"]');
 
-      await Promise.resolve(
-        this.page.click('a[data-test-mobile-nav="members"]')
-      );
-      }catch(error){
-        try{
+        await Promise.resolve(
+          this.page.click('a[data-test-mobile-nav="members"]')
+        );
+      } catch (error) {
+        try {
           await this.page.waitForSelector('a[data-test-nav="members"]');
 
-          await Promise.resolve(
-            this.page.click('a[data-test-nav="members"]')
-          );
-        }catch(error){
+          await Promise.resolve(this.page.click('a[data-test-nav="members"]'));
+        } catch (error) {
           throw error;
         }
       }
@@ -33,7 +31,7 @@ class MembersPage {
       throw error; // Rethrow the error to propagate it to the calling code
     }
   }
-  
+
   async createMember() {
     try {
       // Wait for an element that contains a span with the text "New member"
@@ -47,14 +45,12 @@ class MembersPage {
       await this.page.screenshot({
         path: this.screenshotDirectoryEscenario + "createMember.png",
       });
-      await Promise.resolve(
-        this.page.click('button[data-test-button="save"]')
-      );
+      await Promise.resolve(this.page.click('button[data-test-button="save"]'));
       await this.page.waitForTimeout(2000);
       await Promise.resolve(
         this.page.click('a[data-test-link="members-back"]')
       );
-      await this.page.waitForSelector('h2.gh-canvas-title');
+      await this.page.waitForSelector("h2.gh-canvas-title");
 
       await this.page.screenshot({
         path: this.screenshotDirectoryEscenario + "membersPage.png",
@@ -72,7 +68,41 @@ class MembersPage {
         }
       }
       if (!memberEncontrado) {
-        throw "no se encontro el el member creado";
+        throw "no se encontro el member creado";
+      }
+      await this.page.waitForTimeout(1000);
+      return this.page;
+    } catch (error) {
+      console.error("Visit Scheduled Post Page failed:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+
+  async createMemberWithoutMail_Error() {
+    try {
+      // Wait for an element that contains a span with the text "New member"
+      await this.page.waitForSelector('a[data-test-new-member-button="true"]');
+      await this.page.click('a[data-test-new-member-button="true"]');
+      await this.page.waitForSelector('input[data-test-input="member-name"]');
+      const nameMember = faker.person.firstName();
+      await this.page.keyboard.type(nameMember);
+      await Promise.resolve(this.page.click('button[data-test-button="save"]'));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "createMember.png",
+      });
+      await this.page.waitForTimeout(2000);
+      const pElements = await this.page.$$eval("p.response", (ps) =>
+        ps.map((p) => p.textContent)
+      );
+      let errorEncontrado = false;
+      for (let i = 0; i < pElements.length; i++) {
+        if (pElements[i].includes("Please enter an email.")) {
+          errorEncontrado = true;
+          return;
+        }
+      }
+      if (!errorEncontrado) {
+        throw "no se encontro el error esperado";
       }
       await this.page.waitForTimeout(1000);
       return this.page;
