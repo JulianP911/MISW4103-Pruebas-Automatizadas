deca75671c33e4d144fc315343afbca7a53e04d5
Laura Cabra <lh.cabra@uniandes.edu.co>
2023-11-15 21:29:16 -0500

    version pruebas para 4.44.0

---
+++ Semana 6/PuppeteerGhost4-44-0/PuppeteerTester/README.md
@@ -0,0 +1,35 @@
+# Puppeteer Tester
+
+Aplicativo para hacer pruebas a Ghost con Puppeteer
+
+### Prerequisitos
+- Descargar y crear un usuario administrador en ghost (v5.69.1) (Link de cómo instalar: https://ghost.org/docs/install/)
+- Clonar el repositorio de PuppeteerTester
+- Tener instalado un IDE para revisar y configurar el proyecto (se recomienda Visual studio code)
+
+### Versión de node recomendada
+`Node v18.18.2`
+
+### Configuración del Aplicativo
+#### Atributos
+- ghostUrl: url en el que esta escuchando ghost (ej: http://localhost:2368/ghost/)
+- userEmail: correo de usuario de ghost previamente creado (ej: prueba@prueba.com)
+- userPassword: contraseña de usuario de ghost previamente creado (ej: prueba12345)
+- timeout: tiempo de espera entre tareas para la correcta renderización de los componentes, este valor varia dependiendo de las características del equipo en el que se ejecute el aplicativo (ej. 10000)
+
+### Pasos de ejecución
+- Iniciar Ghost
+`ghost start`
+- Ubicarse en la raiz del proyecto (*\Semana 5\PuppeteerTester) 
+- Configurar el archivo config.json de acuerdo a los atributos propios configurados en Ghost
+- Instalar las dependencias del proyecto:
+`npm install`
+- ejecutarlo:
+`node index.js`
+
+### Lectura de resultado
+- En el momento en el que se ejecuten las pruebas se vera en el terminal el estado en el que termina cada una de ellas (las pruebas se ejecutan secuencial, una tras otra)
+- En la carpeta del proyecto se creara una carpeta screenshots en donde se encuentra una carpeta por fecha y por cada uno de los escenarios, alli se encontrara las imagenes de las principales pantallas recorridas
+
+### Resultado de pruebas
+![image](https://github.com/JulianP911/Pruebas-Automatizadas/assets/142235794/0ca5a6f1-03c9-43d2-b955-a127ab91c40f)
---
+++ Semana 6/PuppeteerGhost4-44-0/PuppeteerTester/pagesPage.js
@@ -0,0 +1,345 @@
+const { faker } = require("@faker-js/faker");
+let config = require("./config.json");
+
+const timeoutConfig = config.timeout;
+
+class PagesPage {
+  constructor(page, ghostUrl, screenshotDirectoryEscenario) {
+    this.page = page;
+    this.ghostUrl = ghostUrl;
+    this.screenshotDirectoryEscenario = screenshotDirectoryEscenario;
+  }
+  async visit() {
+    try {
+      try {
+        await this.page.screenshot({
+          path: this.screenshotDirectoryEscenario + "visitPage.png",
+        });
+        await this.page.waitForSelector(
+          `div[role='button'].gh-mobile-nav-bar-more`
+        );
+        await Promise.resolve(
+          this.page.click(`div[role='button'].gh-mobile-nav-bar-more`)
+        );
+        await this.page.waitForTimeout(timeoutConfig);
+        await this.page.screenshot({
+          path: this.screenshotDirectoryEscenario + "moreNavBar.png",
+        });
+        await Promise.resolve(this.page.click('a[data-test-nav="pages"]'));
+      } catch (error) {
+        try {
+          await this.page.waitForSelector('a[data-test-nav="pages"]');
+
+          await Promise.resolve(this.page.click('a[data-test-nav="pages"]'));
+        } catch (error) {
+          throw error;
+        }
+      }
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "PagesPage.png",
+      });
+      await this.page.waitForTimeout(timeoutConfig);
+    } catch (error) {
+      console.error("Visit pages Page failed:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+
+  async createPage() {
+    try {
+      // Wait for an element that contains a span with the text "New Page"
+      await this.page.waitForSelector("a[data-test-new-page-button]");
+      await this.page.click("a[data-test-new-page-button]");
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "newPage.png",
+      });
+      await this.page.waitForSelector("textarea[data-test-editor-title-input]");
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "fillTitle.png",
+      });
+      await this.page.keyboard.press("Tab");
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "completePage.png",
+      });
+      await this.page.waitForTimeout(timeoutConfig);
+      await Promise.resolve(
+        this.page.click('button[data-test-button="publish-flow"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "publishClick.png",
+      });
+      await this.page.waitForSelector('button[data-test-button="continue"]');
+      await Promise.resolve(
+        this.page.click('button[data-test-button="continue"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "continuePublish.png",
+      });
+      await this.page.waitForSelector(
+        'button[data-test-button="confirm-publish"]'
+      );
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "confirmNewPage.png",
+      });
+      await Promise.resolve(
+        this.page.click('button[data-test-button="confirm-publish"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      const element = await this.page.$(
+        '.gh-publish-title[data-test-publish-flow="complete"]'
+      );
+      if (element) {
+        console.log("Create page successfully");
+      } else {
+        throw "Create page failed";
+      }
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "createPagesPage.png",
+      });
+      this.page.waitForSelector(
+        'button[data-test-button="close-publish-flow"]'
+      );
+
+      await Promise.resolve(
+        this.page.click('button[data-test-button="close-publish-flow"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      this.page.waitForSelector('.gh-btn-editor[data-test-link="pages"]');
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "backPageForm.png",
+      });
+      await Promise.resolve(
+        this.page.click('.gh-btn-editor[data-test-link="pages"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "backPages.png",
+      });
+      return this.page;
+    } catch (error) {
+      console.error("Create page faile:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+
+  async createDraft(titlePage) {
+    try {
+      // Wait for an element that contains a span with the text "New Page"
+      await this.page.waitForSelector("a[data-test-new-page-button]");
+      await this.page.click("a[data-test-new-page-button]");
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "newPageDraft.png",
+      });
+      await this.page.waitForSelector("textarea[data-test-editor-title-input]");
+      await this.page.keyboard.type(titlePage);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "titleDraft.png",
+      });
+      await this.page.keyboard.press("Tab");
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "completeDraftForm.png",
+      });
+      await this.page.waitForTimeout(timeoutConfig);
+      this.page.waitForSelector('.gh-btn-editor[data-test-link="pages"]');
+
+      await Promise.resolve(
+        this.page.click('.gh-btn-editor[data-test-link="pages"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.waitForSelector("h3.gh-content-entry-title", {
+        timeout: timeoutConfig,
+      });
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "listPages.png",
+      });
+      const h3Elements = await this.page.$$eval(
+        "h3.gh-content-entry-title",
+        (h3s) => h3s.map((h3) => h3.textContent)
+      );
+      let tituloEncontrado = false;
+      for (let i = 0; i < h3Elements.length; i++) {
+        if (h3Elements[i].includes(titlePage)) {
+          tituloEncontrado = true;
+          return;
+        }
+      }
+      if (!tituloEncontrado) {
+        throw "no se encontro el titulo del draft en el listado de pages";
+      }
+      return this.page;
+    } catch (error) {
+      console.error("Create draft page faile:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+
+  async createPageScheduled() {
+    try {
+      // Wait for an element that contains a span with the text "New Page"
+      await this.page.waitForSelector("a[data-test-new-page-button]");
+      await this.page.click("a[data-test-new-page-button]");
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "newPageScheduled.png",
+      });
+      await this.page.waitForSelector("textarea[data-test-editor-title-input]");
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "titleScheduled.png",
+      });
+      await this.page.keyboard.press("Tab");
+      await this.page.keyboard.type(faker.lorem.sentence(2));
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "completeScehduledForm.png",
+      });
+      await this.page.waitForTimeout(timeoutConfig);
+      await Promise.resolve(
+        this.page.click('button[data-test-button="publish-flow"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "publishScheduled.png",
+      });
+      await this.page.evaluate(() => {
+        document.querySelector("button.gh-publish-setting-title").click();
+      });
+      await this.page.waitForSelector('div[data-test-radio="schedule"]', {
+        timeout: timeoutConfig,
+      });
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "settingScheduled.png",
+      });
+      await this.page.evaluate(() => {
+        document.querySelector('div[data-test-radio="schedule"]').click();
+      });
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "scheduleNewPage.png",
+      });
+      await this.page.waitForSelector('button[data-test-button="continue"]');
+      await Promise.resolve(
+        this.page.click('button[data-test-button="continue"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.waitForSelector(
+        'button[data-test-button="confirm-publish"]'
+      );
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "confirmNewSceduled.png",
+      });
+      await Promise.resolve(
+        this.page.click('button[data-test-button="confirm-publish"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      const element = await this.page.$(
+        '.gh-publish-title[data-test-publish-flow="complete"]'
+      );
+      if (element) {
+        console.log("Page creado exitosamente");
+      } else {
+        throw "No se encontro componente de creacion exitosa";
+      }
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "createPageScheduled.png",
+      });
+      this.page.waitForSelector(
+        'button[data-test-button="close-publish-flow"]'
+      );
+
+      await Promise.resolve(
+        this.page.click('button[data-test-button="close-publish-flow"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      this.page.waitForSelector('.gh-btn-editor[data-test-link="pages"]');
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "backScheduled.png",
+      });
+      await Promise.resolve(
+        this.page.click('.gh-btn-editor[data-test-link="pages"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "backlistScheduled.png",
+      });
+      return this.page;
+    } catch (error) {
+      console.error("Create page faile:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+
+  async editDraft(titlePage, newTitlePage) {
+    try {
+      await this.page.evaluate(async (titlePage) => {
+        const elements = document.querySelectorAll(".gh-content-entry-title");
+        for (const element of elements) {
+          if (element.textContent.trim() === titlePage.trim()) {
+            await element.click();
+          }
+        }
+        return null;
+      }, titlePage);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "editPage.png",
+      });
+      await this.page.waitForSelector("textarea[data-test-editor-title-input]");
+      await this.page.evaluate(() => {
+        const element = document.querySelector(
+          "textarea[data-test-editor-title-input]"
+        );
+        element.value = "";
+        element.focus();
+      });
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "emptyTitle.png",
+      });
+      await this.page.keyboard.type(newTitlePage);
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "editedPage.png",
+      });
+      await this.page.waitForSelector(
+        '.gh-btn-editor[data-test-link="pages"]',
+        { timeout: timeoutConfig }
+      );
+
+      await Promise.resolve(
+        this.page.click('.gh-btn-editor[data-test-link="pages"]')
+      );
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.waitForSelector("h3.gh-content-entry-title", {
+        timeout: timeoutConfig,
+      });
+
+      const h3Elements = await this.page.$$eval(
+        "h3.gh-content-entry-title",
+        (h3s) => h3s.map((h3) => h3.textContent)
+      );
+      let tituloEncontrado = false;
+      for (let i = 0; i < h3Elements.length; i++) {
+        if (h3Elements[i].includes(newTitlePage)) {
+          tituloEncontrado = true;
+          return;
+        }
+      }
+      if (!tituloEncontrado) {
+        throw "no se encontro el titulo del draft en el listado de pages";
+      }
+
+      await this.page.waitForTimeout(timeoutConfig);
+      await this.page.screenshot({
+        path: this.screenshotDirectoryEscenario + "finalEditPage.png",
+      });
+      return this.page;
+    } catch (error) {
+      console.error("Edit draft Pages failed:", error.message);
+      throw error; // Rethrow the error to propagate it to the calling code
+    }
+  }
+}
+
+module.exports = PagesPage;
