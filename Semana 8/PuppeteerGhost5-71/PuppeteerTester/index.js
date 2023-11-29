e491e46ff094cce33f6c0d066083f91e536c90d2
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2023-11-29 16:58:06 -0500

    add 1 - 6 scenarios

--- Semana 8/PuppeteerGhost5-71/PuppeteerTester/index.js
+++ Semana 8/PuppeteerGhost5-71/PuppeteerTester/index.js
@@ -25,11 +25,331 @@ const ensureDirectoryExists = (directoryPath) => {
 };
 ensureDirectoryExists(screenshotDirectory);
 const runScenarios = async () => {
+  await runScenario1();
+  await runScenario2();
+  await runScenario3();
+  await runScenario4();
+  await runScenario5();
+  await runScenario6();
   await runScenario7();
   await runScenario8();
   await runScenario9();
   await runScenario10();
   await runScenario11();
+};
+
+/**
+ * Escenario 1: Como usuario administrador actualizo el facebook asociado a ghost
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en editar social accounts
+ * And Se escribe la url de la cuenta que se desea asociar en el campo de facebook
+ * And Se da click en save
+ * Then Se verifica que la url de la cuenta sea la que se escribio
+ */
+const runScenario1 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario1/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en editar social accounts
+     * And Se escribe la url de la cuenta que se desea asociar
+     * And Se da click en save */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseUpdateFacebookURL = await Promise.resolve(settingsPage.updateFacebookURL(valueGenerator.generateWord()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que la url de la cuenta de Facebook sea la que se escribio
+    if (responseUpdateFacebookURL.status) {
+      console.log("E1-Test Passed ");
+    } else {
+      console.log("E1-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E1-Test Failed ");
+  }
+};
+
+/**
+ * Escenario 2: Como usuario administrador actualizo el X asociado a ghost
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en editar social accounts
+ * And Se escribe la url de la cuenta que se desea asociar en el campo de X
+ * And Se da click en save
+ * Then Se verifica que la url de la cuenta sea la que se escribio
+ */
+const runScenario2 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario2/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en editar social accounts
+     * And Se escribe la url de la cuenta que se desea asociar
+     * And Se da click en save */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseUpdateXURL = await Promise.resolve(settingsPage.updateXURL(valueGenerator.generateWord()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que la url de la cuenta de X sea la que se escribio
+    if (responseUpdateXURL.status) {
+      console.log("E2-Test Passed ");
+    } else {
+      console.log("E2-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E2-Test Failed ");
+  }
+};
+
+/**
+ * Escenario 3: Como usuario administrador actualizo X card
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en editar X card
+ * And Se escribe el titulo y descripcion deseados
+ * And Se da click en save
+ * Then Se verifica que la X card tenga las informacion suministrada
+ */
+const runScenario3 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario3/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en editar X card
+     * And Se escribe el titulo y descripcion deseados
+     * And Se da click en save */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseUpdateXCard = await Promise.resolve(settingsPage.updateXCard(valueGenerator.generateWord(),valueGenerator.generateWord()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que la X card tenga las informacion suministrada
+    if (responseUpdateXCard.status) {
+      console.log("E3-Test Passed ");
+    } else {
+      console.log("E3-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E3-Test Failed ");
+  }
+};
+
+/**
+ * Escenario 4: Como usuario administrador actualizo Facebook card
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en editar Facebook card
+ * And Se escribe el titulo y descripcion deseados
+ * And Se da click en save
+ * Then Se verifica que la Facebook card tenga las informacion suministrada
+ */
+const runScenario4 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario4/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en editar Facebook card
+     * And Se escribe el titulo y descripcion deseados
+     * And Se da click en save */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseUpdateFacebookCard = await Promise.resolve(settingsPage.updateFacebookCard(valueGenerator.generateWord(),valueGenerator.generateWord()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que la Facebook card tenga las informacion suministrada
+    if (responseUpdateFacebookCard.status) {
+      console.log("E4-Test Passed ");
+    } else {
+      console.log("E4-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E4-Test Failed ");
+  }
+};
+
+/**
+ * Escenario 5: Como usuario administrador actualizo el Site Timezone
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en editar Site timezone
+ * And Se selecciona la timezone deseada
+ * And Se da click en save
+ * Then Se verifica que la timezone sea la suministrada
+ */
+const runScenario5 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario5/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en editar Site timezone
+     * And Se selecciona la timezone deseada
+     * And Se da click en save */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseUpdateTimezone = await Promise.resolve(settingsPage.updateTimeZone(valueGenerator.generateWord()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que la timezone sea la suministrada
+    if (responseUpdateTimezone.status) {
+      console.log("E5-Test Passed ");
+    } else {
+      console.log("E5-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E5-Test Failed ");
+  }
+};
+
+/**
+ * Escenario 6: Como usuario administrador agrego una recomendación
+ *
+ * Given Se ingresa a la página correspondiente a login
+ * When Se ingresa a settings
+ * And Se da click en add recommendation
+ * And Se digital la url de la recomendación
+ * And Se da click en next
+ * And Se da click en add
+ * Then Se verifica que recomendación agregada este en el listado
+ */
+const runScenario6 = async () => {
+  try {
+    const screenshotDirectoryEscenario = `./screenshots/${timestamp}/Escenario6/`;
+    ensureDirectoryExists(screenshotDirectoryEscenario);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const loginPage = new LoginPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    //Given Se ingresa a la página correspondiente a login
+    await loginPage.visit();
+    await loginPage.login(userEmail, userPassword);
+    /* When Se ingresa a settings
+     * And Se da click en add recommendation
+     * And Se digital la url de la recomendación
+     * And Se da click en next
+     * And Se da click en add */
+    const settingsPage = new SettingsPage(
+      page,
+      ghostUrl,
+      screenshotDirectoryEscenario
+    );
+    await Promise.resolve(settingsPage.visit());
+    const responseAddRecommendation = await Promise.resolve(settingsPage.addRecommendation(valueGenerator.getURLYoutube()));
+
+    // Close the browser after completing the tests
+    await browser.close();
+    // Then Se verifica que recomendación agregada este en el listado
+    if (responseAddRecommendation.status) {
+      console.log("E6-Test Passed ");
+    } else {
+      console.log("E6-Test Failed ");
+    }
+  } catch (e) {
+    console.log(e);
+    console.log("E6-Test Failed ");
+  }
 };
 
 /**
